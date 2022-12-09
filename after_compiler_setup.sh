5f5e175e64c047a21614a6049b4a072003195ac0
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2022-12-09 14:12:19 +0100

    trying different image building strategy

---
+++ after_compiler_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get purge --autoremove -y
+rm -rf /var/lib/apt/lists/*
---
+++ before_compiler_setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DEBIAN_FRONTEND=noninteractive
+
+add-apt-repository ppa:git-core/ppa
+./kitware-archive.sh
+
+apt-get update
+apt-get upgrade -y
+apt-get install -y \
+    build-essential \
+    ccache \
+    cmake \
+    curl \
+    doxygen \
+    ghostscript \
+    git \
+    graphviz \
+    libboost-all-dev \
+    libfftw3-dev \
+    libhdf5-mpi-dev \
+    libopenmpi-dev \
+    ncurses-term \
+    python3 \
+    python3-matplotlib \
+    python3-numpy \
+    python3-pip \
+    python3-scipy \
+    software-properties-common \
+    sudo \
+    vim \
+    wget
+
